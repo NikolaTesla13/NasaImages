611398b61b268ea89e3982aed1cf096ce0dbfde7
Asandei Stefan <52822996+NikolaTesla13@users.noreply.github.com>
2020-05-10 18:15:46 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,9 +4,12 @@ async function main() {
     var raw_data = await fetch(`https://images-api.nasa.gov/search?q=${search_term}&media_type=image`); 
     var data = await raw_data.json();
     var html_tags = "";
+    if(data.collection.items.length <= 0) {
+        html_tags += "<h1>ERROR 404</h1>";
+    }
     for(var i=0;i<data.collection.items.length;i++) {
         console.log(data);
-        html_tags += `<br><img src='${data.collection.items[i].links[0].href}'><br><h2>${data.collection.items[i].data[0].date_created}, ${data.collection.items[i].data[0].title}</h2><h3>${data.collection.items[i].data[0].description}</h3><br><br>`
+        html_tags += `<br><img src='${data.collection.items[i].links[0].href}'><br><h2>${data.collection.items[i].data[0].date_created}, ${data.collection.items[i].data[0].title}</h2><h3>${data.collection.items[i].data[0].description.slice(0, 100)}</h3><br><br>`
     }
     document.getElementById("data").innerHTML = html_tags;
 }
