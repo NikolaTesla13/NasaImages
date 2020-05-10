519ed9789b0c50df7131cde6ed0a992ce0f87118
Asandei Stefan <52822996+NikolaTesla13@users.noreply.github.com>
2020-05-10 14:11:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+async function main() {
+    var search_term = document.getElementById("search-box").value;
+
+    var raw_data = await fetch(`https://images-api.nasa.gov/search?q=${search_term}&media_type=image`); 
+    var data = await raw_data.json();
+    var html_tags = "";
+    for(var i=0;i<data.collection.items.length;i++) {
+        console.log(data);
+        html_tags += `<br><img src='${data.collection.items[i].links[0].href}'><br><h2>${data.collection.items[i].data[0].date_created}, ${data.collection.items[i].data[0].title}</h2><h3>${data.collection.items[i].data[0].description}</h3><br><br>`
+    }
+    document.getElementById("data").innerHTML = html_tags;
+}
